9b2fbb3dcb67a866105d773ae0e616abd990a8dc
Piotr Podgorski <peter.podgorski@gmail.com>
2021-05-20 20:16:48 +0200

    Driver interface definition.

--- test_bank_account.py
+++ test_bank_account.py
@@ -1,23 +1,38 @@
 import pytest
+from money import Money
+
+
+class Driver:
+    def create_account(self, initial_deposit: Money) -> None:
+        pass
+
+    def withdraw(self, amount: Money) -> None:
+        pass
+
+    def check_balance(self) -> Money:
+        return Money(0, "PLN")
 
 
 class PrivateBankingDSL:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, driver: Driver) -> None:
+        self._driver = driver
 
     def have(self, amount: str, currency: str) -> None:
-        pass
+        money = Money(amount, currency)
+        self._driver.create_account(initial_deposit=money)
 
     def withdraw_money(self, amount: str, currency: str) -> None:
-        pass
+        money = Money(amount, currency)
+        self._driver.withdraw(amount=money)
 
     def assert_have(self, amount: str, currency: str) -> None:
-        assert False
+        balance: Money = self._driver.check_balance()
+        assert balance == Money(amount, currency)
 
 
 @pytest.fixture
 def private_banking() -> PrivateBankingDSL:
-    return PrivateBankingDSL()
+    return PrivateBankingDSL(driver=Driver())
 
 
 def test_withdraw_money(private_banking: PrivateBankingDSL):
